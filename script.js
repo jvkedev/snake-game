cc9c762e3cbba5cf1240f49b19aaa12ea356d1de
JVKE <shubhambrown1@gmail.com>
2025-06-20 12:38:53 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,175 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "#1f2937";
+const snakeColor = "#10b981";
+const snakeBorder = "#065f46";
+const foodColor = "#ef4444";
+const unitSize = 25;
+
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+
+let snake = [
+  { x: unitSize * 4, y: 0 },
+  { x: unitSize * 3, y: 0 },
+  { x: unitSize * 2, y: 0 },
+  { x: unitSize, y: 0 },
+  { x: 0, y: 0 },
+];
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
+function gameStart() {
+  running = true;
+  scoreText.textContent = score;
+  createFood();
+  drawFood();
+  nextTick();
+}
+
+function nextTick() {
+  if (running) {
+    setTimeout(() => {
+      clearBoard();
+      drawFood();
+      moveSnake();
+      drawSnake();
+      checkGameOver();
+      nextTick();
+    }, 75);
+  } else displayGameOver();
+}
+
+function clearBoard() {
+  ctx.fillStyle = boardBackground;
+  ctx.fillRect(0, 0, gameWidth, gameHeight);
+}
+
+function createFood() {
+  function randomFood(min, max) {
+    const randNum =
+      Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+    return randNum;
+  }
+  foodX = randomFood(0, gameWidth - unitSize);
+  foodY = randomFood(0, gameHeight - unitSize);
+}
+
+function drawFood() {
+  ctx.fillStyle = foodColor;
+  ctx.fillRect(foodX, foodY, unitSize, unitSize);
+}
+
+function moveSnake() {
+  const head = {
+    x: snake[0].x + xVelocity,
+    y: snake[0].y + yVelocity,
+  };
+  snake.unshift(head);
+
+  // Check if food is eaten
+  if (snake[0].x === foodX && snake[0].y === foodY) {
+    score += 1;
+    scoreText.textContent = score;
+    createFood();
+  } else {
+    snake.pop();
+  }
+}
+
+function drawSnake() {
+  snake.forEach((snakePart, index) => {
+    if (index === 0) {
+      ctx.fillStyle = "#22d3ee"; // Head color
+      ctx.strokeStyle = "#0e7490";
+    } else {
+      ctx.fillStyle = index % 2 === 0 ? "#34d399" : "#10b981"; // Alternate body color
+      ctx.strokeStyle = "#064e3b";
+    }
+
+    ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+    ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+  });
+}
+
+function changeDirection(event) {
+  const keyPressed = event.keyCode;
+  const LEFT = 37,
+    UP = 38,
+    RIGHT = 39,
+    DOWN = 40;
+
+  const goingUp = yVelocity === -unitSize;
+  const goingDown = yVelocity === unitSize;
+  const goingRight = xVelocity === unitSize;
+  const goingLeft = xVelocity === -unitSize;
+
+  switch (true) {
+    case keyPressed === LEFT && !goingRight:
+      xVelocity = -unitSize;
+      yVelocity = 0;
+      break;
+    case keyPressed === UP && !goingDown:
+      xVelocity = 0;
+      yVelocity = -unitSize;
+      break;
+    case keyPressed === RIGHT && !goingLeft:
+      xVelocity = unitSize;
+      yVelocity = 0;
+      break;
+    case keyPressed === DOWN && !goingUp:
+      xVelocity = 0;
+      yVelocity = unitSize;
+      break;
+  }
+}
+
+function checkGameOver() {
+  switch (true) {
+    case snake[0].x < 0:
+    case snake[0].x >= gameWidth:
+    case snake[0].y < 0:
+    case snake[0].y >= gameHeight:
+      running = false;
+      break;
+  }
+
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      running = false;
+    }
+  }
+}
+
+function displayGameOver() {
+  ctx.font = "50px Poppins";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
+}
+
+function resetGame() {
+  score = 0;
+  xVelocity = unitSize;
+  yVelocity = 0;
+  snake = [
+    { x: unitSize * 4, y: 0 },
+    { x: unitSize * 3, y: 0 },
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 },
+  ];
+  gameStart();
+}
